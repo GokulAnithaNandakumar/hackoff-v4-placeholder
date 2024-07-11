1bfe763fda66ebd08ecc72d22de10acf0e3bb682
Goutham Kurapati <gouthamkurapati@gmail.com>
2024-07-11 10:09:49 +0530

    Fixed minor bug

--- app/_Components/Navbar/Navbar.jsx
+++ app/_Components/Navbar/Navbar.jsx
@@ -1,21 +1,26 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMobile, setIsMobile] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
     };
+
+    // Set initial value on client side
+    handleResize();
+
+    // Add event listener for window resize
     window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-
 
   return (
     <nav>
