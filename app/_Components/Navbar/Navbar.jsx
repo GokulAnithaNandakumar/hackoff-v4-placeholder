c7609ef9a9f3834cd6e8a63f239671d752de1564
Goutham Kurapati <gouthamkurapati@gmail.com>
2024-07-11 10:01:28 +0530

    Added responsiveness to navbar

--- app/_Components/Navbar/Navbar.jsx
+++ app/_Components/Navbar/Navbar.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+
   return (
     <nav>
-      <ul>
+      <ul className={`${isMobile ? "mobile" : ""}`}>
         <div className="navbar-logo-container">
           <img src="images/logo-small.png" alt="" />
         </div>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        {isMobile ? (
+          <li onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            {isMenuOpen ? (
+              <img src="images/cross.png" alt="x" />
+            ) : (
+              <img src="images/hamburger.png" alt="=" />
+            )}
+          </li>
+        ) : (
+          <>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </>
+        )}
       </ul>
+      {isMobile && isMenuOpen && (
+        <ul className="navbar-menu">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      )}
     </nav>
   );
 };
