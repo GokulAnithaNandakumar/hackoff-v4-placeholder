510d685bf94d6e0a10834b2cb878ff0e1029a9be
Affan Ahammed <116718291+affangitty@users.noreply.github.com>
2024-07-24 17:26:03 +0530

    Create AboutHackoff

---
+++ app/_Components/AboutHackoff/AboutHackoff.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import './AboutHackoff.css';
+
+const AboutHackoff=()=>{
+    return ( <div className="section">
+            <div className="abt">
+                <div className="abth">
+                    <h1>ABOUT HACKOFF</h1>
+                </div>
+                <div className="grid">
+                    <div className="gridin">
+                    <div className="up">
+                        <div className="one">
+                            <div className="oneup">
+                                <h1>About Hackoff</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id fringilla leo, sed pulvinar metus. Duis mattis maximus auctor. Nullam efficitur ligula nulla, in ornare erat euismod et. Mauris hendrerit dui non tempus placerat. Quisque non felis vel erat tristique tincidunt ac ut metus.</p>
+                            </div>
+                            <div className="img1">
+                            <img src="images/phone.png" className="phoneone"/>
+                            </div>
+                        </div>
+                        <div className="two">
+                        <div className="twoup">
+                                <h1>Something Something</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id fringilla leo, sed pulvinar metus. Duis mattis maximus auctor. Nullam efficitur ligula nulla, in ornare erat euismod et.</p>
+                            </div>
+                            <div className="img2">
+                            <img src="images/phone2.png" className="phonetwo"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="down">
+                        <div className="three">
+                            <div className="threeup">
+                                <div className="left">
+                                    <div className="lefttext">
+                                <h1>Something Something</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id fringilla leo, sed pulvinar metus. Duis mattis maximus auctor.</p>
+                                    </div>
+                                </div>
+                                <div className="right">
+                                    <div className="righttext">
+                                    <h1>Something Something</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id fringilla leo, sed pulvinar metus. Duis mattis maximus auctor.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="threedown">
+                            <h1>Something Something</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id fringilla leo, sed pulvinar metus. Duis mattis maximus auctor. Nullam efficitur ligula nulla, in ornare erat euismod et.</p>
+                            </div>
+                        </div>
+                        <div className="four">
+                        <div className="fourup">
+                                <h1>Something Something</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id fringilla leo, sed pulvinar metus. Duis mattis maximus auctor. Nullam efficitur ligula nulla, in ornare erat euismod et.</p>
+                            </div>
+                            <div className="img3">
+                            <img src="images/phone3.png" className="phonethree"/>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+    </div>
+    )
+}
+
+export default AboutHackoff;
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import styles from "./page.module.css";
 import Hero from './_Components/Hero/Hero'
 import Tagline from './_Components/Tagline/Tagline'
 import Footer from "./_Components/Footer/Footer";
+import AboutHackoff from "./_Components/AboutHackoff/AboutHackoff";
 import About from "./_Components/About/About";
 import Faq from "./_Components/Faq/Faq";
 import Banner from "./_Components/Banner/Banner";
@@ -12,6 +13,7 @@ export default function Home() {
     <div className={styles.main}>
       <Hero/>
       <Tagline/>
+      <AboutHackoff/>
       <About/>
       <Faq/>
       <Banner/>
