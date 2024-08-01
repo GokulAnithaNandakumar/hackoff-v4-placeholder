793156901961ac7fd733838af71d4de43520b59d
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2024-08-01 08:10:01 +0530

    faqs added

--- app/_Components/Faq/Faq.jsx
+++ app/_Components/Faq/Faq.jsx
@@ -17,6 +17,33 @@ const Faq = () => {
     setActiveIndex(activeIndex === index ? null : index);
   };
 
+  const faqData = [
+    {
+      question: "WHAT IS HACKOFF?",
+      answer: "Hackoff is a global 48-hour hackathon in collaboration with Major League Hacking (MLH). Participants build innovative projects in areas like Data Science, AI, Robotics, IoT, AR/VR, and Blockchain. Having had over 3,500 participants from 15+ countries, expert mentors, workshops, and exciting prizes, Hackoff is an unmissable event for tech enthusiasts."
+    },
+    {
+      question: "WHO ALL CAN PARTICIPATE?",
+      answer: "Participation is open for absolutely anyone."
+    },
+    {
+      question: "ARE THERE ANY PREREQUISITES OR REQUIRED SKILLS?",
+      answer: "No, there are no prerequisites or required skills. Participants can work on whichever track they want with tools of their choice."
+    },
+    {
+      question: "WHAT SHOULD I BRING TO A HACKATHON?",
+      answer: "Bring your laptop, chargers, and any other tools you might need for coding or project development. It is also recommended to bring snacks, sleeping gear, and personal items."
+    },
+    {
+      question: "WHAT SUPPORT IS AVAILABLE DURING THE HACKATHON?",
+      answer: "Mentors, workshops, and resources will be provided to help participants with technical issues, idea development, and project execution."
+    },
+    {
+      question: "HOW DO I PREPARE FOR A HACKATHON?",
+      answer: "Familiarize yourself with the tracks. Practicing coding, brainstorming project ideas, and assembling a team in advance can be beneficial."
+    },
+  ];
+
   return (
     <div className='container'>
       <h1 className='h1faq'>FAQ'S</h1>
@@ -29,13 +56,13 @@ const Faq = () => {
         {/* Tab / Collapse Section */}
         <div className='faq-tabs-container'>
           <div className='tab-container'>
-            {Array(4).fill().map((_, index) => (
+            {faqData.map((faq, index) => (
               <div className='tab-accordion' key={index}>
                 <div
                   className={`titleWrapper ${activeIndex === index ? 'active' : 'inactive'}`}
                   onClick={() => toggleAccordion(index)}
                 >
-                  <h3>WHAT IS A HACK-OFF EVENT?</h3>
+                  <h3>{faq.question}</h3>
                   <div className="collapse-icon-container">
                     <div className='collapse-icon'>
                       <div className='acc-close'></div>
@@ -48,10 +75,7 @@ const Faq = () => {
                   className={`faq-ans desWrapper ${activeIndex === index ? 'open' : ''}`}
                   style={{ maxHeight: activeIndex === index ? heights[index] : 0 }}
                 >
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit id venenatis pretium risus euismod dictum egestas orci netus feugiat ut egestas ut sagittis tincidunt phasellus elit etiam cursus orci in. Id sed montes.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit id venenatis pretium risus euismod dictum egestas orci netus feugiat ut egestas ut sagittis tincidunt phasellus elit etiam cursus orci in. Id sed montes.
-                  </p>
+                  <p>{faq.answer}</p>
                 </div>
               </div>
             ))}
