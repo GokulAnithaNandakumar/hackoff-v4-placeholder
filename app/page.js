0946ec9fe372e9f404d32577b7d22ff9013ba39f
GokulAnithaNandakumar <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2024-08-03 08:37:22 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import styles from "./page.module.css";
-import Hero from './_Components/Hero/Hero'
-import Tagline from './_Components/Tagline/Tagline'
+import Hero from './_Components/Hero/Hero';
+import Tagline from './_Components/Tagline/Tagline';
 import Footer from "./_Components/Footer/Footer";
 import AboutHackoff from "./_Components/AboutHackoff/AboutHackoff";
 import About from "./_Components/About/About";
@@ -10,27 +10,34 @@ import Banner from "./_Components/Banner/Banner";
 import Contact from "./_Components/Contact/Contact";
 import React from "react";
 import PreLoader from "./_Components/PreLoader/PreLoader";
+import Head from 'next/head';
+import favicon from '../public/images/favicon.ico';
 
 export default function Home() {
   const [loading, setLoading] = React.useState(true); 
   return (
     <>
-    {loading && <PreLoader setLoading={setLoading} />}
-    <div className={styles.main}>
-   {!loading && <div>
-      <div className="hero-marquee">
-        <Hero/>
-        <Tagline/>
+      <Head>
+        <title>HackOff V4.0</title>
+        <link rel="icon" href={favicon}/>
+      </Head>
+      {loading && <PreLoader setLoading={setLoading} />}
+      <div className={styles.main}>
+        {!loading && (
+          <div>
+            <div className="hero-marquee">
+              <Hero/>
+              <Tagline/>
+            </div>
+            <AboutHackoff/>
+            <About/>
+            <Faq/>
+            <Banner/>
+            <Contact/>
+            <Footer/>
+          </div>
+        )}
       </div>
-      <AboutHackoff/>
-      <About/>
-      <Faq/>
-      <Banner/>
-      <Contact/>
-      <Footer/>
-    </div>}
-    </div>
     </>
   );
 }
-
