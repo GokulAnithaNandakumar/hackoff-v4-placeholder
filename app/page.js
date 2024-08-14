b2f5b0d742c9b95aed1b7cd3f9b9cb9d4c877d76
Abhishumat Singh Beniwal <112894631+Abhishumat23@users.noreply.github.com>
2024-08-14 21:09:14 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 import Hero from './_Components/Hero/Hero';
 import Tagline from './_Components/Tagline/Tagline';
 import Footer from "./_Components/Footer/Footer";
-import AboutHackoff from "./_Components/AboutHackoff/AboutHackoff";
 import About from "./_Components/About/About";
 import Faq from "./_Components/Faq/Faq";
 import Banner from "./_Components/Banner/Banner";
