c6e065600417b350b5ff1ade0c599a3ab12cbaa3
Abhishumat Singh Beniwal <112894631+Abhishumat23@users.noreply.github.com>
2024-08-15 13:10:08 +0530

    added lotties to tagline

--- app/_Components/Tagline/Tagline.jsx
+++ app/_Components/Tagline/Tagline.jsx
@@ -1,18 +1,55 @@
-"use client"
-import React, { useRef } from "react";
+"use client";
+import React, { useRef, useEffect } from "react";
 import "./Tagline.css";
 import { useScroll, useTransform, motion } from "framer-motion";
+import lottie from "lottie-web";
 
 const Tagline = () => {
   const element = useRef(null);
+  const innovationLottie = useRef(null);
+  const engageLottie = useRef(null);
+
+  useEffect(() => {
+    let innovationAnimation, engageAnimation;
+
+    if (innovationLottie.current) {
+      innovationAnimation = lottie.loadAnimation({
+        container: innovationLottie.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path: "https://raw.githubusercontent.com/GokulAnithaNandakumar/lottie-host/66ea27f6ac54f4cba84cb146c153e5ae59c080d8/Heart%20Smiley.json",
+      });
+    }
+
+    if (engageLottie.current) {
+      engageAnimation = lottie.loadAnimation({
+        container: engageLottie.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path: "https://raw.githubusercontent.com/GokulAnithaNandakumar/lottie-host/66ea27f6ac54f4cba84cb146c153e5ae59c080d8/Sunglasses%20Smiley.json",
+      });
+    }
+
+    return () => {
+      if (innovationAnimation) {
+        innovationAnimation.destroy();
+      }
+      if (engageAnimation) {
+        engageAnimation.destroy();
+      }
+    };
+  }, []);
 
   const { scrollYProgress } = useScroll({
     target: element,
-    offset: ["start 0.75", "start start"]
+    offset: ["start 0.75", "start start"],
   });
 
   const topText = "Fusing Innovation";
-  const bottomText = "& Technology into Unparalleled Digital Solutions that Inspire and Engage.";
+  const bottomText =
+    "& Technology into Unparalleled Digital Solutions that Inspire and Engage.";
 
   const wrapCharactersInSpan = (text) => {
     return text.split("").map((char, index) => {
@@ -34,8 +71,14 @@ const Tagline = () => {
 
   return (
     <div className="tagline" ref={element}>
-      <div className="tagline-top">{wrapCharactersInSpan(topText)}</div>
-      <div className="tagline-bottom">{wrapCharactersInSpan(bottomText)}</div>
+      <div className="tagline-top">
+        {wrapCharactersInSpan(topText)}
+        <div ref={innovationLottie} className="lottie-animation-sunglasses"></div>
+      </div>
+      <div className="tagline-bottom">
+        {wrapCharactersInSpan(bottomText)}
+        <div ref={engageLottie} className="lottie-animation-heart"></div>
+      </div>
     </div>
   );
 };
